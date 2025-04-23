5cc8af115c23f62a377b4d7525b7f2d6b71aea3e
ypoirier <ypoirier@inria.fr>
2025-04-23 20:43:11 +0200

    Add colmap2nerf script

--- colmap2nerf.py
+++ colmap2nerf.py
@@ -45,6 +45,7 @@ def parse_args():
 	parser.add_argument("--overwrite", action="store_true", help="Do not ask for confirmation for overwriting existing images and COLMAP data.")
 	parser.add_argument("--mask_categories", nargs="*", type=str, default=[], help="Object categories that should be masked out from the training images. See `scripts/category2id.json` for supported categories.")
 	args = parser.parse_args()
+	assert args.keep_colmap_coords, "Make sure to use keep colmap coords"
 	return args
 
 def do_system(arg):
@@ -143,7 +144,7 @@ def variance_of_laplacian(image):
 	return cv2.Laplacian(image, cv2.CV_64F).var()
 
 def sharpness(imagePath):
-	image = cv2.imread(imagePath)
+	image = cv2.imread(imagePath.replace(".jpg", ".png"))
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	fm = variance_of_laplacian(gray)
 	return fm
