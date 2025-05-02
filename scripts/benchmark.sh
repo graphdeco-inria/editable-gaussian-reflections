bb3b26b94c6c2122c38779edfbdcc82b75763e62
HU Jeffrey <jeffrey.hu@inria.fr>
2025-05-02 17:39:43 +0200

    Runs on real scenes.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -9,6 +9,7 @@ RAYTRACER_VERSION="../optix-gaussian-raytracing/build/v74/"
 
 SCENE_DIR="data/renders"
 SCENE_LIST="shiny_kitchen shiny_livingroom shiny_office shiny_bedroom"
+# SCENE_LIST="multichromeball_kitchen_v2 multichromeball_identical_kitchen_v2 multichromeball_tint_kitchen_v2 multichromeball_value_kitchen_v2"
 OUTPUT_DIR="output/benchmark_v74"
 
 for SCENE in $SCENE_LIST;
@@ -24,6 +25,7 @@ do
         -s $SCENE_DIR/$SCENE \
         -m $OUTPUT_DIR/$SCENE \
         -r $RESOLUTION \
+        --eval \
         --skip_video \
         --raytracer_version $RAYTRACER_VERSION
 done
--- scripts/benchmark_real.sh
+++ scripts/benchmark_real.sh
@@ -2,23 +2,24 @@
 set -xe
 
 RESOLUTION=512
-RAYTRACER_VERSION="../optix-gaussian-raytracing/build/v52/"
+RAYTRACER_VERSION="../optix-gaussian-raytracing/build/v74/"
 
-SCENE_DIR="data/real_datasets_v1/renders_priors"
-SCENE_LIST="shiny_kitchen shiny_livingroom shiny_office shiny_bedroom"
-OUTPUT_DIR="output/benchmark_renders_priors"
+SCENE_DIR="data/real_datasets_v2_filmic/refnerf_priors"
+SCENE_LIST="gardenspheres sedan toycar"
+OUTPUT_DIR="output/benchmark_refnerf_priors"
 
-# SCENE_DIR="data/real_datasets_v1/refnerf_priors"
-# SCENE_LIST="gardenspheres sedan toycar"
-# OUTPUT_DIR="output/benchmark_refnerf_priors"
+# SCENE_DIR="data/real_datasets_v2_filmic/neural_catacaustics_priors"
+# SCENE_LIST="compost concave_bowl2 crazy_blade2 hallway_lamp multibounce silver_vase2 wateringcan2"
+# OUTPUT_DIR="output/benchmark_neural_catacaustics_priors"
 
-# SCENE_DIR="data/real_datasets_v1/360_v2_priors"
+# SCENE_DIR="data/real_datasets_v2_filmic/360_v2_priors"
 # SCENE_LIST="garden bicycle stump bonsai counter kitchen room treehill flowers"
 # OUTPUT_DIR="output/benchmark_360_v2_priors"
 
-# SCENE_DIR="data/real_datasets_v1/neural_catacaustics_priors"
-# SCENE_LIST="compost concave_bowl2 crazy_blade2 hallway_lamp multibounce silver_vase2 wateringcan2"
-# OUTPUT_DIR="output/benchmark_neural_catacaustics_priors"
+# SCENE_DIR="data/real_datasets_v2_filmic/renders_priors"
+# SCENE_LIST="shiny_kitchen shiny_livingroom shiny_office shiny_bedroom"
+# # SCENE_LIST="multichromeball_kitchen_v2 multichromeball_identical_kitchen_v2 multichromeball_tint_kitchen_v2 multichromeball_value_kitchen_v2"
+# OUTPUT_DIR="output/benchmark_renders_priors"
 
 for SCENE in $SCENE_LIST;
 do
@@ -26,13 +27,14 @@ do
         -s $SCENE_DIR/$SCENE \
         -m $OUTPUT_DIR/$SCENE \
         -r $RESOLUTION \
+        --no_znear_densif_pruning \
         --eval \
-        --num_farfield_init_points 100_000 \
         --raytracer_version $RAYTRACER_VERSION
 
     python render.py \
         -s $SCENE_DIR/$SCENE \
         -m $OUTPUT_DIR/$SCENE \
         -r $RESOLUTION \
+        --eval \
         --raytracer_version $RAYTRACER_VERSION
 done
