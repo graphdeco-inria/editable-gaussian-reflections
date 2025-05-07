117da357d07bd6b1504442eceb6c1a11fc9375b4
HU Jeffrey <jeffrey.hu@inria.fr>
2025-05-07 16:29:18 +0200

    Render novel trajectory.

---
+++ render_novel_views.py
@@ -0,0 +1,155 @@
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+
+import os
+from argparse import ArgumentParser
+from copy import deepcopy
+
+import numpy as np
+import torch
+import torchvision
+from tqdm import tqdm
+
+from arguments import ModelParams, OptimizationParams, PipelineParams, get_combined_args
+from gaussian_renderer import GaussianModel, GaussianRaytracer, render
+from scene import Scene
+from scene.tonemapping import tonemap
+from utils.cam_utils import generate_spiral_path
+from utils.general_utils import safe_state
+
+
+@torch.no_grad()
+def render_set(
+    cameras,
+    pipeline,
+    background,
+    raytracer,
+    save_dir,
+):
+    for idx, camera in enumerate(tqdm(cameras, desc="Rendering progress")):
+        raytracer.cuda_module.denoise.copy_(not args.skip_denoiser)
+
+        if args.spp > 1:
+            raytracer.cuda_module.accumulate.copy_(True)
+            raytracer.cuda_module.accumulated_rgb.zero_()
+            raytracer.cuda_module.accumulated_normal.zero_()
+            raytracer.cuda_module.accumulated_sample_count.zero_()
+            for i in range(args.spp):
+                package = render(
+                    camera, raytracer, pipeline, background, blur_sigma=None
+                )
+        else:
+            package = render(camera, raytracer, pipeline, background, blur_sigma=None)
+
+        if args.supersampling > 1:
+            for key, value in package.__dict__.items():
+                batched = value.ndim == 4
+                resized = torch.nn.functional.interpolate(
+                    value[None] if not batched else value,
+                    scale_factor=1.0 / args.supersampling,
+                    mode="area",
+                )
+                setattr(package, key, resized[0] if not batched else resized)
+
+        diffuse_image = tonemap(package.rgb[0]).clamp(0, 1)
+        glossy_image = tonemap(package.rgb[1:-1].sum(dim=0)).clamp(0, 1)
+        pred_image = tonemap(package.rgb[-1]).clamp(0, 1)
+        result = {
+            "render": pred_image,
+            "glossy": glossy_image,
+            "diffuse": diffuse_image,
+            "position": package.position[0],
+            "normal": package.normal[0] / 2 + 0.5,
+            "roughness": package.roughness[0],
+            "F0": package.F0[0],
+        }
+        for k, v in result.items():
+            save_path = os.path.join(save_dir, k, "{0:05d}".format(idx) + f"_{k}.png")
+            if not os.path.isdir(os.path.dirname(save_path)):
+                os.makedirs(os.path.dirname(save_path), exist_ok=True)
+            torchvision.utils.save_image(v, save_path)
+
+
+@torch.no_grad()
+def render_sets(model_params: ModelParams, iteration: int, pipeline: PipelineParams):
+    gaussians = GaussianModel(model_params)
+    scene = Scene(model_params, gaussians, load_iteration=iteration, shuffle=False)
+    views = scene.getTrainCameras()
+    background = torch.tensor([0, 0, 0], dtype=torch.float32, device="cuda")
+
+    raytracer = GaussianRaytracer(
+        gaussians, views[0].image_width, views[0].image_height
+    )
+    if args.spp > 1:
+        raytracer.cuda_module.denoise.fill_(False)
+
+    # Create spiral path from EnvGS
+    camtoworlds_all = []
+    for view in views:
+        w2c = np.eye(4)
+        w2c[:3, :3] = view.R.T
+        w2c[:3, 3] = view.T
+        c2w = np.linalg.inv(w2c)
+        camtoworlds_all.append(c2w)
+    camtoworlds_all = np.array(camtoworlds_all)
+    camtoworlds_all = generate_spiral_path(camtoworlds_all)
+    camtoworlds_all = np.concatenate(
+        [
+            camtoworlds_all,
+            np.repeat(np.array([[[0.0, 0.0, 0.0, 1.0]]]), len(camtoworlds_all), axis=0),
+        ],
+        axis=1,
+    )  # [N, 4, 4]
+
+    # Create cameras from camera path
+    cameras = []
+    for c2w in camtoworlds_all:
+        camera = deepcopy(views[0])
+        w2c = np.linalg.inv(c2w)
+        camera.R = np.transpose(w2c[:3, :3])
+        camera.T = w2c[:3, 3]
+        camera.update()
+        cameras.append(camera)
+
+    save_dir = os.path.join(
+        model_params.model_path, "novel_views", f"ours_{scene.loaded_iter}"
+    )
+    render_set(
+        cameras,
+        pipeline,
+        background,
+        raytracer,
+        save_dir,
+    )
+
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Testing script parameters")
+    model = ModelParams(parser, sentinel=False)
+    _ = OptimizationParams(parser)
+    pipeline = PipelineParams(parser)
+
+    # Rendering args
+    parser.add_argument("--iteration", default=-1, type=int)
+    parser.add_argument("--spp", default=4, type=int)
+    parser.add_argument("--supersampling", default=1, type=int)
+    parser.add_argument("--skip_denoiser", action="store_true")
+    parser.add_argument("--quiet", action="store_true")
+
+    args = get_combined_args(parser)
+    print("Rendering " + args.model_path)
+
+    safe_state(args.quiet)
+
+    model_params = model.extract(args)
+    model_params.resolution *= args.supersampling
+    render_sets(model_params, args.iteration, pipeline.extract(args))
---
+++ utils/cam_utils.py
@@ -0,0 +1,131 @@
+# Copied from here:
+#   https://github.com/zju3dv/EnvGS/blob/main/easyvolcap/utils/cam_utils.py
+
+import numpy as np
+
+def normalize(x):
+    return x / (np.linalg.norm(x, axis=-1, keepdims=True) + 1e-13)
+
+def compute_center_of_attention(c2ws: np.ndarray):
+    # TODO: Should vectorize this to make it faster, this is not very tom94
+    totw = 0.0
+    totp = np.asarray([0.0, 0.0, 0.0], dtype=np.float32)
+    for mf in c2ws:
+        for mg in c2ws:
+            p, w = closest_point_2_lines(mf[:, 3], mf[:, 2], mg[:, 3], mg[:, 2])
+            if w > 0.01:
+                totp += p * w
+                totw += w
+    totp /= totw
+    return totp[..., None]  # 3, 1
+
+
+def closest_point_2_lines(oa: np.ndarray, da: np.ndarray, ob: np.ndarray, db: np.ndarray):
+    # Returns point closest to both rays of form o+t*d, and a weight factor that goes to 0 if the lines are parallel
+    da = da / np.linalg.norm(da)
+    db = db / np.linalg.norm(db)
+    c = np.cross(da, db)
+    denom = np.linalg.norm(c)**2
+    t = ob - oa
+    ta = np.linalg.det([t, db, c]) / (denom + 1e-10)
+    tb = np.linalg.det([t, da, c]) / (denom + 1e-10)
+    return (oa + ta * da + ob + tb * db) * 0.5, denom
+
+def average_c2ws(c2ws: np.ndarray, align_cameras: bool = True, look_at_center: bool = True) -> np.ndarray:
+    """
+    Calculate the average pose, which is then used to center all poses
+    using @center_poses. Its computation is as follows:
+    1. Compute the center: the average of pose centers.
+    2. Compute the z axis: the normalized average z axis.
+    3. Compute axis y': the average y axis.
+    4. Compute x' = y' cross product z, then normalize it as the x axis.
+    5. Compute the y axis: z cross product x.
+    Note that at step 3, we cannot directly use y' as y axis since it's
+    not necessarily orthogonal to z axis. We need to pass from x to y.
+    Inputs:
+        poses: (N_images, 3, 4)
+    Outputs:
+        pose_avg: (3, 4) the average pose
+    """
+
+    if align_cameras:
+        # 1. Compute the center
+        center = compute_center_of_attention(c2ws)[..., 0]  # (3)
+        # 2. Compute the z axis
+        z = -normalize(c2ws[..., 1].mean(0))  # (3) # FIXME: WHY?
+        # 3. Compute axis y' (no need to normalize as it's not the final output)
+        y_ = c2ws[..., 2].mean(0)  # (3)
+        # 4. Compute the x axis
+        x = -normalize(np.cross(z, y_))  # (3)
+        # 5. Compute the y axis (as z and x are normalized, y is already of norm 1)
+        y = -np.cross(x, z)  # (3)
+
+    else:
+        # 1. Compute the center
+        center = c2ws[..., 3].mean(0)  # (3)
+        # 2. Compute the z axis
+        if look_at_center:
+            look = compute_center_of_attention(c2ws)[..., 0]  # (3)
+            z = normalize(look - center)
+        else:
+            z = normalize(c2ws[..., 2].mean(0))  # (3)
+        # 3. Compute axis y' (no need to normalize as it's not the final output)
+        y_ = c2ws[..., 1].mean(0)  # (3)
+        # 4. Compute the x axis
+        x = -normalize(np.cross(z, y_))  # (3)
+        # 5. Compute the y axis (as z and x are normalized, y is already of norm 1)
+        y = -np.cross(x, z)  # (3)
+
+    c2w_avg = np.stack([x, y, z, center], 1)  # (3, 4)
+    return c2w_avg
+
+def generate_spiral_path(c2ws: np.ndarray,
+                         n_render_views=300,
+                         n_rots=2,
+                         zrate=0.5,
+                         percentile=70,
+
+                         focal_offset=0.0,
+                         radius_ratio=1.0,
+                         xyz_ratio=[1.0, 1.0, 0.25],
+                         xyz_offset=[0.0, 0.0, 0.0],
+                         **kwargs) -> np.ndarray:
+    """Calculates a forward facing spiral path for rendering.
+    From https://github.com/google-research/google-research/blob/342bfc150ef1155c5254c1e6bd0c912893273e8d/regnerf/internal/datasets.py
+    and https://github.com/apchenstu/TensoRF/blob/main/dataLoader/llff.py
+    """
+    # Prepare input data
+    c2ws = c2ws[..., :3, :4]
+
+    # Center pose
+    c2w_avg = average_c2ws(c2ws, align_cameras=False, look_at_center=True)  # [3, 4]
+
+    # Get average pose
+    v_up = -normalize(c2ws[:, :3, 1].sum(0))
+
+    # Find a reasonable "focus depth" for this dataset as a weighted average
+    # of near and far bounds in disparity space.
+    focal = focal_offset + np.linalg.norm(compute_center_of_attention(c2ws)[..., 0] - c2w_avg[..., 3])  # (3)
+
+    # Get radii for spiral path using 70th percentile of camera origins.
+    radii = np.percentile(np.abs(c2ws[:, :3, 3] - c2w_avg[..., 3]), percentile, 0) * radius_ratio  # N, 3
+    radii = np.concatenate([xyz_ratio * radii, [1.]])  # 4,
+
+    # Generate c2ws for spiral path.
+    render_c2ws = []
+    for theta in np.linspace(0., 2. * np.pi * n_rots, n_render_views, endpoint=False):
+        t = radii * [np.cos(theta), np.sin(theta), np.sin(theta * zrate), 1.] + \
+            np.concatenate([xyz_offset, [0.]])
+
+        center = c2w_avg @ t
+        center = center.astype(c2ws.dtype)
+        lookat = c2w_avg @ np.array([0, 0, focal, 1.0], dtype=c2ws.dtype)
+
+        v_front = -normalize(center - lookat)
+        v_right = normalize(np.cross(v_front, v_up))
+        v_down = np.cross(v_front, v_right)
+        c2w = np.stack([v_right, v_down, v_front, center], axis=-1)  # 3, 4
+        render_c2ws.append(c2w)
+
+    render_c2ws = np.stack(render_c2ws, axis=0)  # N, 3, 4
+    return render_c2ws
