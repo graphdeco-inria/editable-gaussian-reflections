084bc0a5848a802e4aafc62c83f31210fef43e5d
ypoirier <ypoirier@inria.fr>
2025-06-02 16:47:28 +0200

    clean up scripts

--- run.sh
+++
@@ -1,9 +0,0 @@
-
-if [[ -n "${RENDER_ONLY}" ]]; then
-    ZNEAR=1.0 python render.py "${@}"
-else
-    python train.py "${@}" && 
-    ZNEAR=1.0 python render.py "${@}" && 
-    ZNEAR=1.0 python measure_fps.py "${@}"
-fi &&
-true
--- scripts/queue_comparisons.sh
+++
@@ -1,5 +0,0 @@
-
-
-for scene in shiny_kitchen shiny_office shiny_livingroom shiny_bedroom; do 
-    q a6000 -t 3:00:00 -n $scene <<< "bash run.sh -s renders/$scene -m output_comparisons/$scene -r 768 --raytracer_version /home/ypoirier/optix/gausstracer/build/v90"
-done
