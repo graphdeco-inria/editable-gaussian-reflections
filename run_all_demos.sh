9546a2eee201054d9c28364e40817d8279af5ce9
Yohan Poirier-Ginter <yohanpoirierginter@gmail.com>
2025-10-29 14:19:36 -0400

    added easy scripts for running paper results

--- run_all_demos.sh
+++ run_all_demos.sh
@@ -1,6 +1,6 @@
 
 
-ADD_BOOK_INIT_PTS=1 python train.py output/demos/shiny_office_with_book -s data/demos/shiny_office_with_book "$@" && python render.py -m output/demos/shiny_office_with_book
+SKIP_EVAL=1 ADD_BOOK_INIT_PTS=1 bash run.sh output/demos/shiny_chromeball_with_book -s data/demos/shiny_chromeball_with_book "$@"
 bash run.sh output/demos/multichromeball -s data/demos/multichromeball "$@"
 bash run.sh output/demos/multichromeball_identical -s data/demos/multichromeball_identical "$@"
 bash run.sh output/demos/multichromeball_tint -s data/demos/multichromeball_tint "$@"
--- run_real_scenes.sh
+++
@@ -1,5 +0,0 @@
-
-
-bash run.sh output/renders_predicted/shiny_kitchen -s data/renders_predicted/shiny_kitchen "$@"
-bash run.sh output/renders_predicted/shiny_livingroom  -s data/renders_predicted/shiny_livingroom "$@"
-bash run.sh output/renders_predicted/shiny_office  -s data/renders_predicted/shiny_office "$@"
